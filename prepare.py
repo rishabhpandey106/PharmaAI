13a7baaafaaea35ec81fd4ecdfa2769d28aa0ba6
rishabhpandey106 <rishabhpandey230@gmail.com>
2025-01-27 02:43:11 +0530

    added prepare.py

---
+++ prepare.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain.text_splitter import TextSplitter, RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+DATA_PATH = 'data/'
+def load_pdf(data):
+    loader = DirectoryLoader(data,glob='*.pdf',loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+documents=load_pdf(data=DATA_PATH)
+# print(len(documents))
+
+def create_chunks(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500,
+                                                 chunk_overlap=50)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+text_chunks=create_chunks(extracted_data=documents)
+# print("Length of Text Chunks: ", len(text_chunks))
+
+def get_embedding_model():
+    embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embedding_model
+
+embedding_model=get_embedding_model()
+
+# Step 4: Store embeddings in FAISS
+DB_FAISS_PATH="embeddings/db_faiss"
+db=FAISS.from_documents(text_chunks, embedding_model)
+db.save_local(DB_FAISS_PATH)
